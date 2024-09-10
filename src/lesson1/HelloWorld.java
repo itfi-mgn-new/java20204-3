af9cb996e4c147710ee714393b5426824e209f30
Student <Student@IMAC-201-101-PC.itf.local>
2024-09-10 19:47:50 +0500

    DAily commit

--- src/lesson1/HelloWorld.java
+++ src/lesson1/HelloWorld.java
@@ -1,10 +1,62 @@
 package lesson1;
 
-public class HelloWorld {
+import java.lang.Object;
+import java.lang.System;
 
+public class HelloWorld extends /*java.lang.*/Object  {
+	public int x = 10;
+	private static double y = 20.0;
+	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.err.println("sdsd");
+		/*java.lang.*/System.err.println("sdsd");
+		int	x1 = 10, x2 = 20, x3 = x1 + x2;
+		
+		System.err.println("x1+x2="+(x1+x2));
+		System.err.println("x1-x2="+(x1-x2));
+		System.err.println("x1*x2="+(x1*x2));
+		System.err.println("x1/x2="+(x1/x2));
+		System.err.println("x1%x2="+(x1%x2));
+		
+		System.err.println("x1|x2="+(x1|x2));
+		System.err.println("x1&x2="+(x1&x2));
+		System.err.println("x1^x2="+(x1^x2));
+		System.err.println("~x1="+(~x1));
+
+		System.err.println("x1<<2="+(x1<<2));
+		System.err.println("x1>>2="+((-x1)>>2));
+		System.err.println("x1>>>2="+((-x1)>>>2));
+
+		x3 = x1 + (x2 = x1);
+		x3 += x1;	// x3 = x3 + x1;
+	
+		boolean	b1 = true, b2 = false, b3 = b1 && b2;
+		
+		System.err.println("b1||b2="+(b1||b2));
+		System.err.println("b1&&b2="+(b1&&b2));
+		System.err.println("!b1="+(!b1));
+
+		System.err.println("b1|b2="+(b1|b2));
+		System.err.println("b1&b2="+(b1&b2));
+
+		x1 = x2 > 0 ? x2 : x3;
+	
+		if (x1 == 0) {
+			
+		}
+		
+		//	>
+		//	>=
+		//	<
+		//	<=
+		//	==
+		//	!=
+		
+		byte	z1 = 10, z2 = 20, z3 = (byte)(z1 + z2);
 	}
 
+	public void print() {
+		Object x = null;
+	}
+	
 }
