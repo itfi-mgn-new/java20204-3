efb1adeb55ab340a1e9ed059d43798441bc71cff
Student <Student@IMAC-201-101-PC.itf.local>
2024-10-01 19:51:23 +0500

    Daily commit

---
+++ src/lesson4/LifeCycleTEst.java
@@ -0,0 +1,57 @@
+package lesson4;
+
+public class LifeCycleTEst {
+	static final int	x = 10;	// ConstantValue
+	static final double	y = Math.random();
+
+	final int		z;
+	final double 	t = Math.random();
+	
+	{
+		System.err.println("laskjdfkljdj");
+	}
+	
+	public LifeCycleTEst(/*LifeCycleTEst this */) {
+		// System.err.println("laskjdfkljdj");
+		this(20);
+		//this.z = 20;
+	}
+	
+	public LifeCycleTEst(/*LifeCycleTEst this, */int a) {
+		// System.err.println("laskjdfkljdj");
+		this.z = a;
+	}
+	
+	// public void <init>() {
+	//	 z = 20;
+	//	 t = Math.random();
+	//   System.err.println("laskjdfkljdj");
+	// }
+	
+	// public static void <clinit>() {
+	// 	y = Math.random();
+	// 	System.err.println("BEfore main");
+	// }
+	
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.err.println("Hello world");
+		
+		LifeCycleTEst t = new LifeCycleTEst(/*t,*/100);
+		LifeCycleTEst t2 = new LifeCycleTEst(/*t2,*/200);
+		
+		System.err.println(t.z);
+		System.err.println(t2.z);
+	
+		t = null;
+	}
+
+	static {
+		System.err.println("BEfore main");
+		test();
+	}
+	
+	static private void test() {
+		
+	}
+}
